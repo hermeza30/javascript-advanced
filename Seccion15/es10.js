5a61b09b916fb449bdb8280b70467cd6176794c0
Antonio Hermez <antonio.hermez@MacBookPro-infra0177.fibertel.com.ar>
2021-09-01 18:09:36 -0300

    Es7 8 10

---
+++ Seccion15/es10.js
@@ -0,0 +1,21 @@
+const array=[1,2,3,[4,5]];
+array.flat();
+const array=[1,2,3,[4,5]];
+array.flat();
+const array3=[1,2,3,[[4],5]];
+array2.flat(2)//
+
+
+const entrie=['bob','sally',,,,,,,,,'cindy'];//Hace un clean del array.s
+entrie.flat();
+
+userEmail='             hola';
+userEmail2='hola             ';
+userEmail.trimStart();
+userEmail2.trimEnd();
+
+
+//From Entries
+var userProfile=[['lala',23],['pepe',30],['tony',55]]
+const objfe=Object.fromEntries(userProfile);//Me retorna un objeta haciendo un flaten
+Object.entries(objfe)//Me retorna 3 array debntro de un array
