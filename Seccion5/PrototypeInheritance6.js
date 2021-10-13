4a156d19bda91c06d2148ce86dfbf73304049635
Antonio Hermez <antonio.hermez@fluxit.com.ar>
2021-10-13 17:24:09 -0300

    Prototype

--- Seccion5/PrototypeInheritance6.js
+++ Seccion5/PrototypeInheritance6.js
@@ -1,3 +1,4 @@
 //Solo las funciones tienen prototype property
 typeof Object //Retorna una funcion
 /**Esto es debido a que un Object es una funcion porque la funcion tiene el prototype como property */
+typeof {} //Retorna Object, este es creado desde el contructor del Object base
